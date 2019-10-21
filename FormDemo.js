60f7cacb29afc66f70d190cd722985d2221856f6
Lars-m <lam@cphbusiness.dk>
2019-10-21 18:26:37 +0200

    Create FormDemo.js

---
+++ FormDemo.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+const FormDemo = () => {
+  const initialState = {name: "", age: null, email: ""};
+ 
+  const handleSubmit = (evt) => {}
+
+  const handleInput = (event) => {}
+  
+  return (
+    <div style={{marginTop:25}}>
+      <form onSubmit={handleSubmit} >
+        <input id="name" type="text" 
+               placeholder="Name" />
+        <br/>
+        <input id="age" type="number" 
+               placeholder="Age" />
+        <br/>
+        <input id="email" type="text" 
+                placeholder="email" />
+        <br/>
+        <button >Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default FormDemo;
